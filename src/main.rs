6ca2d97e4c755258b3b47dad0141064127bee3cb
Joey Carr <carr.joey@gmail.com>
2021-01-01 14:56:25 -0500

    Camera class and antialiasing via sampling

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 
-mod ray;
-mod vec3;
+mod camera;
 mod hittable;
+mod ray;
 mod sphere;
+mod vec3;
 
+use camera::Camera;
 use image::{ImageBuffer, RgbImage, Rgb};
 use ray::Ray;
-use vec3::{Color, dot, Point3, unit_vector, Vec3};
+use vec3::{Color, Point3, unit_vector};
 use hittable::{Hittable, HittableList, HitRecord};
+use rand::random;
 use sphere::Sphere;
 
 fn imsave(name: &str, width: usize, height: usize, data: Vec<f64>) {
@@ -30,19 +33,6 @@ fn imsave(name: &str, width: usize, height: usize, data: Vec<f64>) {
 
 }
 
-fn hit_sphere(center: Point3, radius: f64, ray: &Ray) -> f64 {
-    let oc = ray.origin - center;
-    let a = ray.direction.length_squared();
-    let half_b = dot(oc, ray.direction);
-    let c = oc.length_squared() - radius*radius;
-    let discriminant = half_b*half_b - a*c;
-    if discriminant < 0. {
-        return -1.
-    } else {
-        return (-half_b - discriminant.sqrt()) / a;
-    }
-}
-
 fn ray_color(ray: Ray, world: &HittableList) -> Color {
     let mut hit_record = HitRecord::new();
     if world.hit(ray, 0., f64::INFINITY, &mut hit_record) {
@@ -60,6 +50,7 @@ fn main() {
     let aspect_ratio = 16.0/9.0;
     let image_width = 400;
     let image_height = (image_width as f64 / aspect_ratio) as usize;
+    let samples_per_pixel = 100;
 
     // World
     let mut world = HittableList::new();
@@ -67,28 +58,24 @@ fn main() {
     world.add(Box::new(Sphere::new(Point3::new(0., -100.5, -1.), 100.)));
 
     // Camera
-    let viewport_height = 2.0;
-    let viewport_width = aspect_ratio * viewport_height;
-    let focal_length = 1.0;
-
-    let origin = Point3::new(0., 0., 0.);
-    let horizontal = Vec3::new(viewport_width as f64, 0., 0.);
-    let vertical = Vec3::new(0., viewport_height as f64, 0.);
-    let lower_left_corner = origin - horizontal/2. - vertical/2. - Vec3::new(0., 0., focal_length);
+    let camera = Camera::new(16./9.);
 
     // Render
     let mut data: Vec<f64> = Vec::with_capacity(image_width*image_height*3);
 
     for j in (0..image_height).rev() {
         for i in 0..image_width {
-            let u = i as f64 / (image_width-1) as f64;
-            let v = j as f64 / (image_height-1) as f64;
+            let mut pixel_color = Color::new(0., 0., 0.);
+            for s in 0..samples_per_pixel {
+                let u = (i as f64 + random::<f64>()) / (image_width-1) as f64;
+                let v = (j as f64 + random::<f64>()) / (image_height-1) as f64;
 
-            let r = Ray::new(origin, lower_left_corner + u*horizontal + v*vertical - origin);
-            let pixel_color = ray_color(r, &world);
-            data.push(pixel_color.x);
-            data.push(pixel_color.y);
-            data.push(pixel_color.z);
+                let r = camera.get_ray(u, v);
+                pixel_color += ray_color(r, &world);
+            }
+            data.push(pixel_color.x / samples_per_pixel as f64);
+            data.push(pixel_color.y / samples_per_pixel as f64);
+            data.push(pixel_color.z / samples_per_pixel as f64);
         }
     }
 
