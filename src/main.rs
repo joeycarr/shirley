d2b0c879d81b1f7c8e7e574080c393b3e4699709
Joey Carr <carr.joey@gmail.com>
2021-01-29 20:46:07 -0500

    Subsurface not working.

--- src/main.rs
+++ src/main.rs
@@ -279,6 +279,74 @@ fn cornell_smoke() -> HitList {
     objects
 }
 
+fn final_scene() -> HitList {
+    let mut boxes1: HitList = Default::default();
+
+    let ground = Lambertian::from_color(Color::new(0.48, 0.83, 0.53));
+
+    const BOXES_PER_SIDE: usize = 20;
+    for i in 0..BOXES_PER_SIDE {
+        for j in 0..BOXES_PER_SIDE {
+            let w = 100.0f64;
+            let x0: f64 = -1000.0 + (i as f64)*w;
+            let z0: f64 = -1000.0 + (j as f64)*w;
+            let y0 = 0.0f64;
+            let x1 = x0 + w;
+            let y1 = randrange(1.0, 101.0);
+            let z1 = z0 + w;
+
+            boxes1.add(Box3D::new(
+                Point3::new(x0, y0, z0),
+                Point3::new(x1, y1, z1),
+                Arc::clone(&ground),
+            ));
+        }
+    }
+
+    let mut objects: HitList = Default::default();
+
+    objects.add(Arc::new(BVHNode::from_hitlist(&boxes1, 0.0, 1.0)));
+
+    let light = DiffuseLight::new(SolidColor::from_rgb(7.0, 7.0, 7.0));
+    objects.add(XZRect::new(123.0, 423.0, 147.0, 412.0, 554.0, light));
+
+    let center1 = Point3::new(400.0, 400.0, 200.0);
+    let center2 = center1 + Vec3::new(30.0, 0.0, 0.0);
+    let moving_sphere_material = Lambertian::from_color(Color::new(0.7, 0.3, 0.1));
+    objects.add(MovingSphere::new(center1, center2, 0.0, 1.0, 50.0, moving_sphere_material));
+
+    objects.add(Sphere::new(Point3::new(260.0, 150.0, 45.0), 50.0, Dielectric::new(1.5)));
+    objects.add(Sphere::new(Point3::new(0.0, 150.0, 145.0), 50.0, Metal::new(Color::new(0.8, 0.8, 0.9), 1.0)));
+
+    let boundary = Sphere::new(Point3::new(360.0, 150.0, 145.0), 70.0, Dielectric::new(1.5));
+    //objects.add(Arc::clone(&boundary));
+    objects.add(ConstantMedium::new(Arc::clone(&boundary), 0.2, Color::new(0.2, 0.4, 0.9)));
+    let boundary = Sphere::new(Point3::new(0.0, 0.0, 0.0), 5000.0, Dielectric::new(1.5));
+    objects.add(ConstantMedium::new(boundary, 0.0001, Color::new(1.0, 1.0, 1.0)));
+
+    let emat = Lambertian::new(Image::new("textures/earthmap.jpg"));
+    objects.add(Sphere::new(Point3::new(400.0, 200.0, 400.0), 100.0, emat));
+    let pertext = Perlin::new(0.1);
+    objects.add(Sphere::new(Point3::new(220.0, 280.0, 300.0), 80.0, Lambertian::new(pertext)));
+
+    let mut boxes2: HitList = Default::default();
+    let white = Lambertian::from_color(Color::new(0.73, 0.73, 0.73));
+    let ns = 1000;
+    for _ in 0..ns {
+        boxes2.add(Sphere::new(Point3::randrange(0.0, 165.0), 10.0, Arc::clone(&white)));
+    }
+
+    objects.add(
+        Translate::new(
+            RotateY::new(
+                Arc::new(BVHNode::from_hitlist(&boxes2, 0.0, 1.0)),
+                15.0f64.to_radians()),
+            Vec3::new(-100.0, 270.0, 395.0)
+    ));
+
+    objects
+}
+
 fn render(
     world: &HitList,
     camera: &Camera,
@@ -316,7 +384,7 @@ fn main() {
     let aspect_ratio = 1.0;
     let image_width = 600;
     let image_height = (image_width as f64 / aspect_ratio) as usize;
-    let samples_per_pixel = 200;
+    let samples_per_pixel = 10;
     let max_depth = 50;
 
     // World
@@ -326,7 +394,7 @@ fn main() {
     let aperture: f64;
     let background: Color;
 
-    let world: HitList = match 7 {
+    let world: HitList = match 8 {
         1 => {
             lookfrom = Point3::new(13.0 ,2.0 ,3.0);
             lookat = Point3::new(0.0 ,0.0 ,0.0);
@@ -375,13 +443,21 @@ fn main() {
             background = Color::new(0.0, 0.0, 0.0);
             cornell_box()
         }
-        _ => {
+        7 => {
             lookfrom = Point3::new(278.0, 278.0, -800.0);
             lookat = Point3::new(278.0, 278.0, 0.0);
             vfov = 40.0;
             aperture = 0.0;
             background = Color::new(0.0, 0.0, 0.0);
             cornell_smoke()
+        }
+        _ => {
+            lookfrom = Point3::new(478.0, 278.0, -600.0);
+            lookat = Point3::new(278.0, 278.0, 0.0);
+            vfov = 40.0;
+            aperture = 0.0;
+            background = Color::new(0.0, 0.0, 0.0);
+            final_scene()
         }
     };
 
